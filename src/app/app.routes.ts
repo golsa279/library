cedcfc80eb27fcbe2613f681b43b692781f4951a
Golsa279 <gh.bahar.279@gmail.com>
2024-12-16 09:41:34 +0000

    before add material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { BooksComponent } from './+pages/books/books.component';
 import { BorrowsComponent } from './+pages/borrows/borrows.component';
 import { MembersComponent } from './+pages/members/members.component';
+import { LoginComponent } from './+pages/login/login.component';
 
 export const routes: Routes = [
     { path: 'books', component: BooksComponent },
     { path: 'members', component: MembersComponent },
     { path: 'borrows', component: BorrowsComponent },
-    { path: '', redirectTo: '/books', pathMatch: 'full' },
-    { path: '**', redirectTo: '/books' }
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '**', redirectTo: '/login' },
+    { path: 'login', component: LoginComponent}
 ];
